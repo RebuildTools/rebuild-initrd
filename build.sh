5505d4575d302da999837b87cee49a274234aaf8
Liam Haworth <liam.haworth@tesserent.com>
2016-10-04 11:31:48 +1100

    Cleaned up the build script in a major way and implemented the "buildKernel" task to build the Rebuild Agent Linux Kernel

--- build.sh
+++ build.sh
@@ -21,8 +21,19 @@ pushd `dirname $0` > /dev/null
 SCRIPTPATH=`pwd`
 popd > /dev/null
 
+## Global
+[ -z "${OUTPUT_DIR}" ] && OUTPUT_DIRECTORY=$SCRIPTPATH/output
+
+
+## Task: package
 [ -z "${INITRD_SRC}" ] && INITRD_SRC=$SCRIPTPATH/initrd_src
-[ -z "${INITRD_DST}" ] && INITRD_DST=$SCRIPTPATH/output
+
+
+## Task: buildKernel
+KERNEL_SRC_GIT="https://github.com/torvalds/linux.git"
+[ -z "${KERNEL_BUILD_CONFIG}" ] && KERNEL_BUILD_CONFIG=$SCRIPTPATH/linux-kernel.config
+[ -z "${KERNEL_VERSION_TAG}" ] && KERNEL_VERSION_TAG="v4.8"
+[ -z "${KERNEL_TMP_DIR}" ] && KERNEL_TMP_DIR=/tmp/rebuild_linux_kernel_src
 
 #####
 ## Script Helper Functions
@@ -56,9 +67,100 @@ binaryExists() {
 	fi
 }
 
+#// Leverages #binaryExists to check
+#// for a binary but adds debug logging
+#// and error throwing (If the binary doesn't
+#// exist).
+#//
+#// PARAMS:
+#//   $1 - The name of the binary
+checkForBinary() {
+	logDebug "Checking for [${1}] binary"
+
+	if binaryExists "${1}"; then
+		return 0
+	else
+		logFatal "The binary [${1}] doesn't exist, please install it!"
+	fi
+}
+
+#// Counts the number of CPU cores
+#// available on the machine this
+#// script is run on
+getCpuCount() {
+	 cat /proc/cpuinfo | awk '/^processor/{print $3}' | wc -l
+}
+
+#// Runs a shell command and captures
+#// stderr & stdout, once the command 
+#// finishes running it checks the exit
+#// code to see if the command exited with
+#// errors (and if those errors are allowed)
+#//
+#// PARAMS:
+#//   $1 String - The info box message to show while running the command
+#//   $2 String - The command to run
+#//   $3 String - (Optional) A comma seperated list of acceptable non-zero exit codes
+runCmd() {
+	logDebug "${1}"
+        local cmd_output=$($2 2>&1)
+        local cmd_exitcode=$?
+
+        if [ $cmd_exitcode -gt 0 ]; then
+                local isGoodCode=false
+
+                if [ ! -z "${3}" ]; then
+                        for allowedCode in $(echo "${3}" | sed 's/,/ /g'); do
+                                [ $allowedCode == $cmd_exitcode ] && isGoodCode=true
+                        done
+                fi
+
+                if ! $isGoodCode; then
+                        logPanic "Command exited with a non-zero code [$cmd_exitcode] when \"${1}\""
+			logPanic "Command run: ${2}"
+			logFatal "Command output:\n${cmd_output}"
+                fi
+        fi  
+}
+
 #####
 ## Script Tasks
 #####
+
+buildKernel() {
+	logInfo "Building the Linux Kernel"
+
+	checkForBinary "git"
+	checkForBinary "make"
+	
+	[ -d $KERNEL_TMP_DIR ] && logWarn "Temporary build directory for kernel exists, clearing it" && rm -rf $KERNEL_TMP_DIR
+
+	runCmd "Making temporary build directory for Linux Kernel" "mkdir ${KERNEL_TMP_DIR}"
+
+	runCmd "Cloning Linux Kernel source" "git clone ${KERNEL_SRC_GIT} ${KERNEL_TMP_DIR}"
+
+	logDebug "Copying kernel build config to source directory"
+	\cp -f $KERNEL_BUILD_CONFIG $KERNEL_TMP_DIR/.config
+
+	logDebug "Changing directory to source"
+	cd $KERNEL_TMP_DIR
+
+	runCmd "Checking out to the desired kernel version" "git checkout tags/${KERNEL_VERSION_TAG}"
+
+	local buildCoreUse=$(($(getCpuCount)/2))
+	runCmd "Compiling kernel with [${buildCoreUse}] CPUs" "make -j${buildCoreUse}"
+
+	logDebug "Changing back to the previous directory"
+	cd - >/dev/null
+
+	logDebug "Checking for destination directory: ${OUTPUT_DIRECTORY}"
+	[ ! -d $INITRD_DST ] && logWarn "The output directory [${OUTPUT_DIRECTORY}] doesn't exist, making it" && mkdir -p ${OUTPUT_DIRECTORY}
+
+	logDebug "Copying compiled kernel to output directory"
+	\cp -f $KERNEL_TMP_DIR/arch/x86/boot/bzImage $OUTPUT_DIRECTORY/linux-kernel
+
+	logInfo "Finished build the Linux Kernel, final output file: ${OUTPUT_DIRECTORY}/linux-kernel"
+}
 
 #// Packages the intird source into
 #// gzip compressed CPIO archive that
@@ -67,26 +169,23 @@ binaryExists() {
 package() {
 	logInfo "Packaging the final ram disk image"
 
-	logDebug "Checking for [cpio] binary"
-	binaryExists "cpio" || logFatal "The [cpio] binary doesn't exist, please install it!"
-
-	logDebug "Checking for [gzip] binary"
-	binaryExists "gzip" || logFatal "The [gzip] binary doesn't exist, please install it!"
+	checkForBinary "cpio"
+	checkForBinary "gzip"
 
 	logDebug "Checking for source files directory: ${INITRD_SRC}"
 	if [ ! -d $INITRD_SRC ]; then logFatal "The provided source directory [${INITRD_SRC}] doesn't exist!"
 	else logDebug "Changing directories into the source directory" && cd $INITRD_SRC; fi
 
-	logDebug "Checking for destination directory: ${INITRD_DST}"
-	[ ! -d $INITRD_DST ] && logWarn "The output directory [${INITRD_DST}] doesn't exist, making it" && mkdir -p $INITRD_DST
+	logDebug "Checking for destination directory: ${OUTPUT_DIRECTORY}"
+	[ ! -d $INITRD_DST ] && logWarn "The output directory [${OUTPUT_DIRECTORY}] doesn't exist, making it" && mkdir -p ${OUTPUT_DIRECTORY}
 
 	logDebug "Build CPIO package and compressing to output file"
-	find . ! -name '.gitkeep' | cpio -o -H newc 2>/dev/null | gzip > $INITRD_DST/initrd.gz
+	find . ! -name '.gitkeep' | cpio -o -H newc 2>/dev/null | gzip > ${OUTPUT_DIRECTORY}/initrd.gz
 
 	logDebug "Changing back to the previous directory"
 	cd - >/dev/null
 
-	logInfo "Finished packaging the ram disk, final output file: ${INITRD_DST}/initrd.gz"
+	logInfo "Finished packaging the ram disk, final output file: ${OUTPUT_DIRECTORY}/initrd.gz"
 }
 
 #####
@@ -106,14 +205,23 @@ showHelp() {
 	echo -e "compiling of the Rebuild Agent ram disk.\n\n"
 
 	echo "Environment Variables:"
+
+	echo -e "\t=== GLOBAL ==="	
 	printf "\t%-20s - %s\n" "LOGLEVEL" "Sets the logging level of the build script (Default: 4 | 1 - PANIC, 2 - FATAL, 3 - WARN, 4 - INFO, 5 - DEBUG)"
+
+	printf "\n\t=== TASK: %-10s ===\n" "buildKernel"
+	printf "\t%-20s - %s\n" "KERNEL_BUILD_CONFIG" "Defines the path to the Linux Kernel build config (Default: ${SCRIPTPATH}/linux-kernel.config)"
+	printf "\t%-20s - %s\n" "KERNEL_VERSION_TAG" "Defines the git tag of desired Linux Kernel version (Default: v4.8)"
+	printf "\t%-20s - %s\n" "KERNEL_TPM_DIR" "Defines the temp directory used when compiling the kernel (Default: /tmp/rebuild_linux_kernel_src)"
+
+	printf "\n\t=== TASK: %-10s ===\n" "package"
 	printf "\t%-20s - %s\n" "INITRD_SRC" "Defines the location of the ram disk source files (Default: ${SCRIPTPATH}/initrd_src)"
 	printf "\t%-20s - %s\n" "INITRD_DST" "Defines the output location of the packaged initrd file (Default: ${SCRIPTPATH}/output)"
 
-	echo -e "\n"
 
-	echo "Tasks:"
+	echo -e "\nTasks:"
 	printf "\t%-20s - %s\n" "showHelp" "Shows this help message"
+	printf "\t%-20s - %s\n" "buildKernel" "Downloads and compiles the Linux Kernel with the Rebuild Agent kernel configuration"
 	printf "\t%-20s - %s\n" "package" "Packages the ram disk source (initrd_src) into the final ram disk image"
 
 	echo -e "\n\n"
